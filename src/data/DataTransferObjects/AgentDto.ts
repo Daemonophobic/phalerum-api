059cbe33cb160ba7a4d110d4981ecfd960578269
Marijn van Dijk <marijn@stickybits.red>
2024-04-03 20:47:34 +0000

    Added agent creation, retrieval, and deletion

--- src/data/DataTransferObjects/AgentDto.ts
+++ src/data/DataTransferObjects/AgentDto.ts
@@ -1,19 +1,24 @@
-import OsEnum from "../enums/OsEnum";
+import AddedBy from "../enums/AddedByEnum";
+import OS from "../enums/OsEnum";
 
 export default class AgentDto {
     id: number;
+    guid: string;
     agentName: string;
-    addedBy: number;
+    addedBy: AddedBy;
+    addedByGuid: string;
     lastCheckIn: EpochTimeStamp;
     ipAddress: string;
     master: boolean;
     communicationToken: string;
-    os: OsEnum;
+    os: OS;
 
     constructor(data: any) {
         this.id = data.id;
+        this.guid = data.guid;
         this.agentName = data.agentName;
         this.addedBy = data.addedBy;
+        this.addedByGuid = data.addedByGuid;
         this.lastCheckIn = data.lastCheckIn;
         this.ipAddress = data.ipAddress;
         this.master = data.master;
--- src/repositories/AgentRepository.ts
+++ src/repositories/AgentRepository.ts
@@ -13,7 +13,7 @@ export default class AgentRepository extends BaseCrudRepository {
                 if (err) reject(err);
 
                 connection.query(
-                    `SELECT id, agent_name as agentName, added_by as addedBy, last_check_in as lastCheckIn, ip_address as ipAddress, master, communication_token as communicationToken, os FROM ${this.tableNames[0]}`,
+                    `SELECT guid, agent_name as agentName, added_by as addedBy, last_check_in as lastCheckIn, ip_address as ipAddress, master, os FROM ${this.tableNames[0]}`,
                     (err: Error, res: any) => {
                         connection.release();
                         if (err) reject(err);
@@ -24,14 +24,14 @@ export default class AgentRepository extends BaseCrudRepository {
         });
     }
 
-    async getAgent(agentId: Number): Promise<AgentDto>|undefined {
+    async getAgent(guid: string): Promise<AgentDto>|undefined {
         return new Promise((resolve, reject) => {
             this.db.getPool().getConnection((err, connection) => {
                 if (err) reject(err);
 
                 connection.query(
-                `SELECT id, agent_name as agentName, added_by as addedBy, last_check_in as lastCheckIn, ip_address as ipAddress, master, communication_token as communicationToken, os FROM ${this.tableNames[0]} WHERE id = ?`,
-                  [agentId],
+                `SELECT guid, agent_name as agentName, added_by as addedBy, last_check_in as lastCheckIn, ip_address as ipAddress, master, os FROM ${this.tableNames[0]} WHERE guid = ?`,
+                  [guid],
                   (err: Error, res: any) => {
                     connection.release();
                     if (err) reject(err);
@@ -42,4 +42,41 @@ export default class AgentRepository extends BaseCrudRepository {
             });
         });
     }
+
+    async addAgent(agent: Partial<AgentDto>, communicationToken: {data: string, iv: string}): Promise<Partial<AgentDto>>|undefined {
+        return new Promise((resolve, reject) => {
+            this.db.getPool().getConnection((err, connection) => {
+                if (err) reject(err);
+
+                connection.query(
+                    `INSERT INTO ${this.tableNames[0]}(guid, agent_name, added_by, added_by_guid, master, communication_token, os) VALUES(?, ?, ?, ?, ?, ?, ?)`,
+                    [agent.guid, agent.agentName, agent.addedBy, agent.addedByGuid, agent.master, JSON.stringify(communicationToken), agent.os],
+                    (err: Error, res: any) => {
+                        connection.release();
+                        if (err) reject(err);
+                        resolve(agent);
+                    }
+                );
+            });
+        });
+    }
+
+    async deleteAgent(guid: string): Promise<Boolean>|undefined {
+        return new Promise((resolve, reject) => {
+            this.db.getPool().getConnection((err, connection) => {
+                if (err) reject(err);
+
+                connection.query(
+                    `DELETE FROM ${this.tableNames[0]} WHERE guid = ?`,
+                    [guid],
+                    (err: Error, _) => {
+                        if (err) {
+                            connection.release();
+                            reject(err)
+                        }
+                        resolve(true);
+                    });
+                });
+            });
+        }
 }

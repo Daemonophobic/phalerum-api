41ecdfdb66b11c2f94f0b0dade4dbed759cef111
DeanTheProgrammer <filmpjesdvd@gmail.com>
2024-04-03 14:23:31 +0200

    woepsie typo fix

--- src/data/DataTransferObjects/RoleDto.ts
+++ src/data/DataTransferObjects/RoleDto.ts
@@ -2,11 +2,11 @@ import PermissionDto  from "./PermissionDto";
 export default class RoleDto {
     id: number;
     name: string;
-    premissions: PermissionDto[];
+    permissions: PermissionDto[];
 
     constructor(data: any) {
         this.id = data.id;
         this.name = data.name;
-        this.premissions = data.premissions;
+        this.permissions = data.premissions;
     }
 }
